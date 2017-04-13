6645a7bc1f7c70a6dc3a6c160e991b0731ed52b7
richdev135@gmail.com <richdev135@gmail.com>
2017-04-13 15:12:51 -0600

    remove unused files. changes to faq

--- public_html/ir_faqs.php
+++ public_html/ir_faqs.php
@@ -8,8 +8,7 @@
  <body>
  <div id="main_container" class="main_container">
 	
-					<?php include_once('inc_header.php'); ?>
-
+				<?php include_once('inc_header.php'); ?>
 				<?php include_once('inc_menu2.php'); ?>
 
 				<div id="body" class="body">
@@ -35,7 +34,7 @@
 					<a href="#faqs_3" >3. Where is VIASPACE located?</a><br />
 					<a href="#faqs_4" >4. Who is VIASPACE's management team?</a><br />
 					<a href="#faqs_5" >5. Where does VIASPACE's stock trade?</a><br />
-					<a href="#faqs_6" >6. What is VIASPACE’s capital structure?</a><br />
+					<a href="#faqs_6" >6. What is VIASPACE's capital structure?</a><br />
 					<a href="#faqs_7" >7. In what industries does VIASPACE operate and provide solutions?</a><br />
 					<a href="#faqs_8" >8. When is VIASPACE's year-end?</a><br />
 					<a href="#faqs_9" >9. When will VIASPACE announce financial results?</a><br />
@@ -44,29 +43,44 @@
 					</p>
 								
 					<br /><a name="faqs_0"></a><h3>1. What is VIASPACE?</h2>
-					<p>VIASPACE grows Giant King Grass as a low-carbon fuel for electricity generating power plants; for energy pellets; and as a feedstock for bio methane production and cellulosic biofuels, biochemicals and biomaterials. 
+					<p>VIASPACE licenses Giant King<sup>&reg</sup> Grass as a low-carbon fuel for electricity generating power plants; for energy pellets; and as a feedstock for bio methane production and cellulosic biofuels, biochemicals and biomaterials. When cut more frequently, Giant King Grass is an excellent animal feed for cattle, dairy cows, sheep, goats, camels and other animals.
 					Proprietary Giant King Grass is the highest yielding biomass crop in the world. Bio-electricity, biogas, biofuels, biochemicals, bio plastics, and biomaterials all need biomass as feedstock, and Giant King Grass is suitable for these applications. Because of its high yield, it is a low-cost feedstock. Equally important, Giant King Grass provides the reliable and well-characterized feedstock that is required by banks and investors in order to provide financing for these bioenergy projects. Giant King Grass is a natural plant, and is not genetically modified and not an invasive species.
+					<br/><br/>
+					VIASPACE provides engineering and support functions for customers including business plans, financial models, agricultural operations support, engineering studies, feasibility studies, and operations plans. VIASPACE partners can deliver and commission direct combustion power plants and anaerobic digester biogas power plants.
 					</p><a href="#top">Top</a><hr /><br /></p>
 					
 					<a name="faqs_2"></a><h3>2. When and where was VIASPACE formed?</h2>
-					<p>VIASPACE&rsquo;s predecessor company, ViaSpace Technologies LLC was formed under the laws of Delaware in 1998. In June 2005, ViaSpace Technologies LLC merged into Global-Wide Publication, Ltd., a corporation formed under the laws of Nevada, and changed its name to VIASPACE Inc.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					<p>VIASPACE's predecessor company, ViaSpace Technologies LLC was formed under the laws of Delaware in 1998. In June 2005, ViaSpace Technologies LLC became a corporation formed under the laws of Nevada, and changed its name to VIASPACE Inc.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					
 					<a name="faqs_3"></a><h3>3. Where is VIASPACE located?</h2>
-					<p>VIASPACE’s executive offices are at 382 N. Lemon Ave., Suite 364, Walnut, CA 91789.<br /><br /><a href="#top">Top</a><hr /><br /></p>
-					<a name="faqs_4"></a><h3>4. Who is VIASPACE's management team?</h2>
-					<p>Dr. Carl Kukkonen is Chairman and CEO. Mr. Stephen Muzi is Chief Financial Officer.<br /><br /><a href="au_management.php"><b>Click here</b></a> for management biographies.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					<p>The VIASPACE's address is 382 N. Lemon Ave., Suite 364, Walnut, CA 91789.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+
+					<a name="faqs_4"></a><h3>4. Who is on VIASPACE's management team?</h2>
+					<p>Dr. Kevin Schewe is Chairman of the board. The  CEO and board member is Mr. Haris Basit. Dr. Carl Kukkonen is Chief Technology Officer and board member. Mr. Stephen Muzi is Chief Financial Officer.<br /><br /><a href="au_management.php"><b>Click here</b></a> for management biographies.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					
 					<a name="faqs_5"></a><h3>5. Where does VIASPACE's stock trade?</h2>
-					<p>VIASPACE’s stock currently trades on the OTC Markets under the symbol VSPC (OTCQB: VSPC).<br /><br /><a href="#top">Top</a><hr /><br /></p>
-					<a name="faqs_6"></a><h3>6. What is VIASPACE’s capital structure?</h2>
-					<p>The authorized capital of VIASPACE is 1,500,000,000 shares of common stock and 10,000,000 shares of preferred stock.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					<p>VIASPACE's stock currently trades on the OTC Markets under the symbol VSPC (OTCQB: VSPC).<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					
+					<a name="faqs_6"></a><h3>6. What is VIASPACE's capital structure?</h2>
+					<p>The authorized capital of VIASPACE is 3,900,000,000 shares of common stock and 10,000,000 shares of preferred stock.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+		
 					<a name="faqs_7"></a><h3>7. In what industries does VIASPACE operate and provide solutions?</h2>
-					<p>VIASPACE provides Giant King<sup>&trade;</sup> Grass seedlings and technical expertise to qualified bioenergy projects that need a low-cost and reliable fuel or feedstock. VIASPACE will also serve as a project developer or codeveloper for power plant or pellet mill projects, together with local partners that have land and requirements for electricity, heat, pellets, biogas or biofuels. VIASPACE and its partners have the capability to deliver an integrated Giant King Grass plantation and biomass power plant project in 24 months. VIASPACE has financial models for power plants and pellet mills.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					<p>
+					VIASPACE provides Giant King Grass planting material  and technical expertise to qualified bioenergy projects that need a low-cost and reliable fuel or feedstock. VIASPACE will also serve as a project developer or co-developer for power plant or pellet mill projects, together with local partners that have land and requirements for electricity, heat, pellets, biogas or biofuels. VIASPACE provides engineering and support functions for customers including business plans, financial models, agricultural operations support, engineering studies, feasibility studies, and operations plans. VIASPACE partners can deliver and commission direct combustion power plants and anaerobic digester biogas power plants. 
+					<br/><br/>
+					VIASPACE also provides Giant King Grass to customers wanting to grow it for animal feed.
+					<br /><br /><a href="#top">Top</a><hr /><br /></p>
+		
 					<a name="faqs_8"></a><h3>8. When is VIASPACE's year-end?</h2>
-					<p>VIASPACE&#39;s fiscal year ends December 31.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					<p>VIASPACE's fiscal year ends December 31.<br /><br /><a href="#top">Top</a><hr /><br /></p>
+		
 					<a name="faqs_9"></a><h3>9. When will VIASPACE announce financial results?</h2>
 					<p>VIASPACE generally reports its financial results six weeks after the close of the quarter in interim quarters and 12 weeks following the close of its year-end. <br /><br /><a href="#top">Top</a><hr /><br /></p>					
+		
 					<a name="faqs_10"></a><h3>10. Who is VIASPACE's transfer agent?</h2>
 					<p>The Nevada Agency and Transfer Company<br />50 West Liberty Street, Suite 880<br />Reno, Nevada 89501<br />(775) 322-0626
 					<br /><br /><a href="#top">Top</a><hr /><br /></p>
+					
 					<a name="faqs_11"></a><h3>11. How do I contact VIASPACE's Investor Relations Department?</h2>
 					<p>Dr. Jan Vandersande, Director of Communications can be contacted at <a href="mailto:ir@viaspace.com">ir@viaspace.com</a> or by telephone at 1-800-517-8050.<br /><br /><a href="#top">Top</a><br /></p>
 
